cb06b2d23f3e4136e9a4bf2560dcfe47bcf886ca
kimiscool12345 <mayotoluoke@gmail.com>
2025-08-17 19:25:14 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var a = 8;
+var b = 23;
+var c = 23; // same value as b 
+var d = null;
+var e; // undefined variable 
+
+document.write("a and b are equal: " + (a == b));
+document.write("<br>");
+document.write("b and c are equal: " + (b == c));
+document.write("<br>");
+document.write("d is null: " + (d === null));
+document.write("<br>");
+document.write("e is undefined: " + (e === undefined));
